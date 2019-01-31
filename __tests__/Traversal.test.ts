8f1266af9a575ea2c0c8dd3469c1fc7964265d49
David Abram <david@abram.com>
2019-01-31 12:35:35 +0100

    updated test

--- __tests__/Traversal.test.ts
+++ __tests__/Traversal.test.ts
@@ -7,43 +7,66 @@ describe('Traversal', () => {
   test('advanced traversal', (done) => {
     const preOrderDepthFirst = TraversalFactory.createTraversal(TraversalType.PreOrderDepthFirst) as PreOrderDepthFirstTraversal;
     const rootNode = new TreeNode(
-      { ['testData']: 1},
+      {
+        testData: 1,
+      },
       null,
     );
     const node1 = new TreeNode(
-      { ['testData']: 1},
+      {
+        testData: 1,
+      },
       rootNode,
     );
     const node2 = new TreeNode(
-      { ['testData']: 1},
+      {
+        testData: 1,
+      },
       rootNode,
     );
     const node3 = new TreeNode(
-      { ['testData']: 3},
+      { 
+        testData: 3,
+        secret: '0cb9e395b1d5db417a2a806dec19d76c',
+      },
       rootNode,
     );
     const node11 = new TreeNode(
-      { ['testData']: 1},
+      {
+        testData: 1,
+      },
       node1,
     );
     const node12 = new TreeNode(
-      { ['testData']: 1},
+      {
+        testData: 1,
+      },
       node1,
     );
     const node13 = new TreeNode(
-      { ['testData']: 3},
+      { 
+        testData: 3,
+        secret: '5f1903f5f2cb32acb4c1dcae9e30d374',
+      },
       node1,
     );
     const node21= new TreeNode(
-      { ['testData']: 3},
+      { 
+        testData: 3,
+        secret: '418b561ab56ef2c6756c3e78d4a67831',
+      },
       node2,
     );
     const node22 = new TreeNode(
-      { ['testData']: 1},
+      {
+        testData: 1,
+      },
       node2,
     );
     const node31 = new TreeNode(
-      { ['testData']: 1},
+      {
+        testData: 1,
+      },
       node3,
     );
 
@@ -57,7 +80,7 @@ describe('Traversal', () => {
     ));
 
     preOrderDepthFirst.setAction((data, height) => {
-      console.log(height, data);
+      console.log(height, data.secret);
     });
 
     expect(() => {
